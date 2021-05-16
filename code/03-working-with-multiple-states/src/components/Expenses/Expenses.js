7c5d3ea062467259e57441dd77cca9e04aa6109c
Mustafa Husrevoglu <muhammed.husrevoglu@prodabilisim.com>
2021-05-16 12:45:02 +0300

    Continue with my branch

--- code/03-working-with-multiple-states/src/components/Expenses/Expenses.js
+++ code/03-working-with-multiple-states/src/components/Expenses/Expenses.js
@@ -5,30 +5,22 @@ import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
-  );
+    let items = (
+        props.items.map(item => {
+            return(
+                <ExpenseItem
+                    title={item.title}
+                    amount={item.amount}
+                    date={item.date}
+                />
+            )
+        })
+    );
+    return (
+        <Card className="expenses">
+            {items}
+        </Card>
+    );
 }
 
 export default Expenses;
